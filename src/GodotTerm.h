3822f98ade9fbc9c7f28506b5321a4129bfbcb2a
John Pallister <john@synchromesh.com>
2025-02-27 17:22:29 +1300

    clang-format troubleshooting

--- src/GodotTerm.h
+++ src/GodotTerm.h
@@ -1,8 +1,9 @@
 /* GodotTerm.h - Steel Bank Common Lisp as a Godot Engine extension.
  * File Created: 6 January 2025
  * Author: John Pallister <john@synchromesh.com>
+ * Ref: https://github.com/synchromesh/GodotTerm
+ * Ref: https://ngake.net
  * Ref: https://github.com/asmaloney/GDExtensionTemplate
- * Ref: https://docs.godotengine.org/en/latest/tutorials/scripting/gdextension/gdextension_cpp_example.html#creating-a-simple-plugin
  *
  */
 
@@ -13,17 +14,17 @@
 #include <godot_cpp/classes/object.hpp>
 
 class GodotTerm : public godot::Object {
-	GDCLASS( GodotTerm, godot::Object )
+  GDCLASS(GodotTerm, godot::Object)
 
-protected:
-	static void _bind_methods();
+ protected:
+  static void _bind_methods();
 
-public:
-	static godot::String version();
-	static godot::String godotCppVersion();
+ public:
+  static godot::String version();
+  static godot::String godotCppVersion();
 
-	//GodotTerm();
-	//~GodotTerm();
+  // GodotTerm();
+  //~GodotTerm();
 };
 
 #endif
--- src/RegisterExtension.cpp
+++ src/RegisterExtension.cpp
@@ -1,67 +1,80 @@
-/* RegisterExtension.cpp
+/* RegisterExtension.cpp - registration & initialisation code for GodotTerm.
  * File Created: 12 February 2025
  * Author: John Pallister <john@synchromesh.com>
+ * Ref: https://github.com/synchromesh/GodotTerm
+ * Ref: https://ngake.net
  * Ref: https://github.com/asmaloney/GDExtensionTemplate
- * Ref: https://docs.godotengine.org/en/latest/tutorials/scripting/gdextension/gdextension_cpp_example.html#creating-a-simple-plugin
  *
  */
 
 #include <gdextension_interface.h>
-#include <godot_cpp/godot.hpp>
-#include <godot_cpp/core/defs.hpp>
+
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
 
 #include "GodotTerm.h"
 
-/// @file
-/// Register our GodotTerm class(es) with Godot.
+/*!
+ * @file
+ * Register our GodotTerm class(es) with Godot.
+ */
 
 namespace {
-	using namespace godot;
-	
-	/// @brief Called by Godot to let us register our classes with Godot.
-	///
-	/// @param level the level being initialized by Godot
-	///
-	/// @see GDExtensionInit
-	void initializeExtension(ModuleInitializationLevel level) {
-		if (level == MODULE_INITIALIZATION_LEVEL_SCENE) {
-			ClassDB::register_class<GodotTerm>();
-		}
-	}
+using namespace godot;
 
-	/// @brief Called by Godot to let us do any cleanup.
-	///
-	/// @see GDExtensionInit
-	void uninitializeExtension(ModuleInitializationLevel level) {
-		if (level == MODULE_INITIALIZATION_LEVEL_SCENE) {
-			// pass
-		}
-	}
+/*!
+ * @brief Called by Godot to let us register our classes with Godot.
+ *
+ * @param level the level being initialized by Godot
+ *
+ * @see GDExtensionInit
+ */
+void initializeExtension(ModuleInitializationLevel level) {
+  if (level == MODULE_INITIALIZATION_LEVEL_SCENE) {
+    ClassDB::register_class<GodotTerm>();
+  }
 }
 
+/*!
+ * @brief Called by Godot to let us do any cleanup.
+ *
+ * @see GDExtensionInit
+ */
+void uninitializeExtension(ModuleInitializationLevel level) {
+  if (level == MODULE_INITIALIZATION_LEVEL_SCENE) {
+    // pass
+  }
+}
+}  // namespace
+
 extern "C" {
-	/// @brief This is the entry point for the shared library.
-	///
-	/// @note The name of this function must match the "entry_symbol" in
-	/// templates/template.*.gdextension.in
-	///
-	/// @param get_proc_address the interface (need more info)
-	/// @param library the library (need more info)
-	/// @param initialization the intialization (need more info)
-	///
-	/// @returns GDExtensionBool
-	GDExtensionBool GDE_EXPORT GDExtensionInit(GDExtensionInterfaceGetProcAddress get_proc_address,
-																						 GDExtensionClassLibraryPtr library,
-																						 GDExtensionInitialization *initialization) {
-		GDExtensionBinding::InitObject init_obj(get_proc_address, library, initialization);
+/*!
+ * @brief This is the entry point for the shared library.
+ *
+ * @note The name of this function must match the "entry_symbol" in
+ * templates/template.*.gdextension.in
+ *
+ * @param get_proc_address the interface (need more info)
+ * @param library the library (need more info)
+ * @param initialization the intialization (need more info)
+ *
+ * @returns GDExtensionBool
+ */
+GDExtensionBool GDE_EXPORT
+GDExtensionInit(GDExtensionInterfaceGetProcAddress get_proc_address,
+                GDExtensionClassLibraryPtr library,
+                GDExtensionInitialization *initialization) {
+  GDExtensionBinding::InitObject init_obj(get_proc_address, library,
+                                          initialization);
 
-		init_obj.register_initializer(initializeExtension);
-		init_obj.register_terminator(uninitializeExtension);
-		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+  init_obj.register_initializer(initializeExtension);
+  init_obj.register_terminator(uninitializeExtension);
+  init_obj.set_minimum_library_initialization_level(
+      MODULE_INITIALIZATION_LEVEL_SCENE);
 
-		return init_obj.init();
-	}
+  return init_obj.init();
+}
 }
 
 /*** End of RegisterExtension.cpp ***/
